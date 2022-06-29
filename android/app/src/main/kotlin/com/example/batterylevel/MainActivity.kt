e4ccc77fdb41cfa0da976b0d091c7657f3ea82fa
shimizu-saffle <shimizu.saffle@gmail.com>
2022-06-29 22:21:18 +0900

    feat: Android 端末でバッテリーレベルを表示

--- android/app/src/main/kotlin/com/example/batterylevel/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/batterylevel/MainActivity.kt
@@ -1,6 +1,45 @@
 package com.example.batterylevel
 
+import android.content.*
 import io.flutter.embedding.android.FlutterActivity
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import android.os.BatteryManager
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+import android.content.Intent
 
 class MainActivity: FlutterActivity() {
+    // Flutter で定義している MethodChannel の引数と一致させる
+    private val BATTERY_CHANNEL = "kentaShimizu.com/battery"
+    private lateinit var channel: MethodChannel
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, BATTERY_CHANNEL)
+
+        /// Receive data from Flutter
+        channel.setMethodCallHandler { call, result ->
+            if (call.method == "getBatteryLevel") {
+                val arguments = call.arguments() as Map<String, String>?
+                val name = arguments?.get("name")
+                val batteryLevel = getBatteryLevel()
+                result.success("$name says: $batteryLevel%")
+            }
+        }
+    }
+
+    private fun getBatteryLevel(): Int {
+        val batteryLevel: Int
+        if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+            val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+        } else {
+            val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+            batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+        }
+        return batteryLevel
+    }
 }
